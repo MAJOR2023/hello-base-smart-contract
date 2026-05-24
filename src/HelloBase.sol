53b38dc48fe155e5c3e122c4431a4fc007d74483
MAJOR2023 <eduardduyun@gmail.com>
2026-05-24 15:54:57 +0700

    feat: add updateMessage function with access control

--- src/HelloBase.sol
+++ src/HelloBase.sol
@@ -11,4 +11,10 @@ contract HelloBase {
         message = _initialMessage;
         owner = msg.sender;
     }
+    
+    function updateMessage(string memory _newMessage) public {
+        require(msg.sender == owner, "Only owner can update message");
+        message = _newMessage;
+        emit MessageUpdated(_newMessage, msg.sender, block.timestamp);
+    }
 }
