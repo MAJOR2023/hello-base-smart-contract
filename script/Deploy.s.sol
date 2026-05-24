71b3b14eefce1d0293b4429d80ab7e6b6131234b
MAJOR2023 <eduardduyun@gmail.com>
2026-05-24 16:31:05 +0700

    feat: add run() function and private key loading

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,4 +5,9 @@ import {Script, console} from "forge-std/Script.sol";
 import {HelloBase} from "../src/HelloBase.sol";
 
 contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerPrivateKey);
+    }
 }
