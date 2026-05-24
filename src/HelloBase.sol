d5436bc693a78f6f2c6440de4eec3045c9e0e720
MAJOR2023 <eduardduyun@gmail.com>
2026-05-24 15:54:38 +0700

    feat: add MessageUpdated event

--- src/HelloBase.sol
+++ src/HelloBase.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.19;
 contract HelloBase {
     string public message;
     address public owner;
+    
+    event MessageUpdated(string newMessage, address indexed updatedBy, uint256 timestamp);
 }
