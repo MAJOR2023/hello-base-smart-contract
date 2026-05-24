dd55115be7f2809886263efbac80328cfddac3e1
MAJOR2023 <eduardduyun@gmail.com>
2026-05-24 15:55:07 +0700

    feat: add getContractInfo and isOnBase view functions

--- src/HelloBase.sol
+++ src/HelloBase.sol
@@ -17,4 +17,16 @@ contract HelloBase {
         message = _newMessage;
         emit MessageUpdated(_newMessage, msg.sender, block.timestamp);
     }
+    
+    function getContractInfo() public view returns (
+        string memory msg,
+        address own,
+        uint256 chainId
+    ) {
+        return (message, owner, block.chainid);
+    }
+    
+    function isOnBase() public view returns (bool) {
+        return block.chainid == 8453 || block.chainid == 84532;
+    }
 }
