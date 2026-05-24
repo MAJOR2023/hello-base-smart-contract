bb3301834cddff0da83aa3bd69bcd4a6854bde29
MAJOR2023 <eduardduyun@gmail.com>
2026-05-24 15:59:31 +0700

    feat: add setUp function with test fixtures

--- test/HelloBase.t.sol
+++ test/HelloBase.t.sol
@@ -5,4 +5,13 @@ import {Test, console} from "forge-std/Test.sol";
 import {HelloBase} from "../src/HelloBase.sol";
 
 contract HelloBaseTest is Test {
+    HelloBase public helloBase;
+    address public owner;
+    address public user;
+    
+    function setUp() public {
+        owner = address(this);
+        user = address(0x123);
+        helloBase = new HelloBase("Initial Message");
+    }
 }
